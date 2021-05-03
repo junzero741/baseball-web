6327e7166943e1932d7cff0ad27036802d2460c6
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-05-03 19:53:04 +0900

    chore: 프로젝트 초기화

---
+++ BE/baseball-be/src/main/java/kr/codesquad/baseball/BaseballBeApplication.java
@@ -0,0 +1,12 @@
+package kr.codesquad.baseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseballBeApplication.class, args);
+    }
+}
