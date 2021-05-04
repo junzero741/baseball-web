06f708251707a465d18548b59ba91152f0964af6
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-05-04 17:06:16 +0900

    feat: GameController에 cors 어노테이션 추가
    추후 설정으로 분리하는 것 고려

--- BE/baseball-be/src/main/java/kr/codesquad/baseball/game/GameController.java
+++ BE/baseball-be/src/main/java/kr/codesquad/baseball/game/GameController.java
@@ -1,12 +1,14 @@
 package kr.codesquad.baseball.game;
 
 import kr.codesquad.baseball.utils.SampleDataFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class GameController {
     @GetMapping("/games")
     public List<GameReadAllResponse> readAll() {
