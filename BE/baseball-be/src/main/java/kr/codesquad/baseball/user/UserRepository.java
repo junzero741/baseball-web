30a77f1b6f6c3784a55de527b4c8278d15a94927
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-05-11 18:15:34 +0900

    fix: 레포지토리 derrived 쿼리 메소드 명 변경

--- BE/baseball-be/src/main/java/kr/codesquad/baseball/user/UserRepository.java
+++ BE/baseball-be/src/main/java/kr/codesquad/baseball/user/UserRepository.java
@@ -3,5 +3,5 @@ package kr.codesquad.baseball.user;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    User findOneBy(long id);
+    User findUserById(long id);
 }
