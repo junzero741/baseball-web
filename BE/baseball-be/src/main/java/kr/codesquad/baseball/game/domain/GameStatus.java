94bc9d709c71c17227f6eea39cccd1930ecdb8b4
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-05-12 16:22:45 +0900

    feat: 게임 추가

---
+++ BE/baseball-be/src/main/java/kr/codesquad/baseball/game/domain/GameStatus.java
@@ -0,0 +1,5 @@
+package kr.codesquad.baseball.game.domain;
+
+public enum GameStatus {
+    PLAYING
+}
---
+++ BE/baseball-be/src/test/java/kr/codesquad/baseball/game/domain/GameRepositoryTest.java
@@ -0,0 +1,22 @@
+package kr.codesquad.baseball.game.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class GameRepositoryTest {
+    @Autowired
+    GameRepository gameRepository;
+
+    @Test
+    void save() {
+        Game game = new Game(GameStatus.PLAYING, new TeamParticipateGame(1L, 2L));
+
+        Game result = gameRepository.save(game);
+
+        System.out.println(gameRepository.findAll());
+    }
+}
