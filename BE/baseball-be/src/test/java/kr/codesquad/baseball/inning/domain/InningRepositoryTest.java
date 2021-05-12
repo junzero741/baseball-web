199a32f6482bacbf0cbea7ee7ef3d3bcdc4fbad1
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-05-12 16:49:37 +0900

    feat: inning 별 점수 update 추가
    복합키에 맞도록 업데이트 쿼리 추가

--- BE/baseball-be/src/test/java/kr/codesquad/baseball/inning/domain/InningRepositoryTest.java
+++ BE/baseball-be/src/test/java/kr/codesquad/baseball/inning/domain/InningRepositoryTest.java
@@ -14,13 +14,13 @@ class InningRepositoryTest {
 
     @Test
     void save() {
-        GameInning gameInning = new GameInning(1, 1L, 1L, 0);
 
         inningRepository.save(1L, 1L, 1L, 0L);
         inningRepository.save(2L, 1L, 1L, 0L);
 
         System.out.println(inningRepository.findAll());
 
+        inningRepository.updateScore(2L, 1L, 1L, 1L);
         System.out.println(inningRepository.findTopByGameIdAndTeamIdOrderByInningDesc( 1L, 1L));
     }
 }
