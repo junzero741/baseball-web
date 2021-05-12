0c5ac7aeb4e20a41432e59640f33dec3c64ba7e1
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-05-13 00:02:08 +0900

    feat: UserControlTeam 추가
    Team에 사용자 제어 여부에 따라 다른 인스턴스 사용할 수 있도록 이너 클래스 생성. 인터페이스로 빼는 방안 고려 필요

--- BE/baseball-be/src/main/java/kr/codesquad/baseball/team/domain/TeamRepository.java
+++ BE/baseball-be/src/main/java/kr/codesquad/baseball/team/domain/TeamRepository.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface TeamRepository extends CrudRepository<Team, Long> {
     List<Team> findAll();
+
+    Team findTeamById(long id);
 }
--- BE/baseball-be/src/test/java/kr/codesquad/baseball/team/domain/TeamRepositoryTest.java
+++ BE/baseball-be/src/test/java/kr/codesquad/baseball/team/domain/TeamRepositoryTest.java
@@ -2,7 +2,10 @@ package kr.codesquad.baseball.team.domain;
 
 import kr.codesquad.baseball.player.domain.Player;
 import kr.codesquad.baseball.player.domain.PlayerRepository;
+import kr.codesquad.baseball.user.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +17,16 @@ import java.util.stream.LongStream;
 @SpringBootTest
 @Transactional
 class TeamRepositoryTest {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     TeamRepository teamRepository;
 
     @Autowired
     PlayerRepository playerRepository;
+
+    @Autowired
+    UserRepository userRepository;
 
     @Test
     void save() {
@@ -27,5 +35,21 @@ class TeamRepositoryTest {
         team.addPlayers(players);
 
         teamRepository.save(team);
+    }
+
+    @Test
+    void addUser() {
+        Team team = teamRepository.findTeamById(1L).addUser(userRepository.findUserById(1L));
+        teamRepository.save(team);
+    }
+
+    @Test
+    void removeUser() {
+        Team team = teamRepository.findTeamById(1L).addUser(userRepository.findUserById(1L));
+        teamRepository.save(team);
+
+        logger.debug("remove start");
+
+        teamRepository.save(team.removeUser());
     }
 }
