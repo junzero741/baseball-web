02384afde772eba45c53d9752c720a5020d2defc
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-05-12 18:17:08 +0900

    feat: PlateAppearance 추가

--- BE/baseball-be/src/main/java/kr/codesquad/baseball/inning/domain/GameInning.java
+++ BE/baseball-be/src/main/java/kr/codesquad/baseball/inning/domain/GameInning.java
@@ -1,6 +1,12 @@
 package kr.codesquad.baseball.inning.domain;
 
+import kr.codesquad.baseball.inning.PlateAppearance;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class GameInning {
     @Id
@@ -10,15 +16,32 @@ public class GameInning {
     private long teamId;
     private int score;
 
+    @MappedCollection(idColumn = "GAME_INNING_ID", keyColumn = "PLATE_APPEARANCE_NUMBER")
+    private List<PlateAppearance> plateAppearances = new ArrayList<>();
+
     public GameInning(int inning, long gameId, long teamId) {
         this.inning = inning;
         this.gameId = gameId;
         this.teamId = teamId;
     }
 
+    public GameInning addNewPlateAppearanceBy(long hitterId) {
+        plateAppearances.add(new PlateAppearance(hitterId, plateAppearances.size() + 1));
+        return this;
+    }
+
     public GameInning updateScore(int score) {
         this.score = score;
         return this;
+    }
+
+    public GameInning outCurrentHitter() {
+        currentPlateAppearance().out();
+        return this;
+    }
+
+    public PlateAppearance currentPlateAppearance() {
+        return plateAppearances.get(plateAppearances.size() - 1);
     }
 
     public Long getId() {
@@ -41,13 +64,19 @@ public class GameInning {
         return score;
     }
 
+    public List<PlateAppearance> getPlateAppearances() {
+        return plateAppearances;
+    }
+
     @Override
     public String toString() {
         return "GameInning{" +
-                "inning=" + inning +
+                "id=" + id +
+                ", inning=" + inning +
                 ", gameId=" + gameId +
                 ", teamId=" + teamId +
                 ", score=" + score +
+                ", plateAppearances=" + plateAppearances +
                 '}';
     }
 }
